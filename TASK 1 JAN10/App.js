44a744654cadde9996875bcaad53b71af384839f
Vignesh1223 <97233834+Vignesh1223@users.noreply.github.com>
2022-01-11 12:09:19 +0530

    FOOTER AND HEADER AND HOME

---
+++ TASK 1 JAN10/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import Home from './Home.js';
+
+
+
+ function App() {
+  return (
+    <div>
+      <h1>TODAY TRANNING SECTION!</h1>
+      <p>TASK :</p>
+      <Header /><Footer /><Home />
+    </div>
+  );
+}
+export default App;
+
+
